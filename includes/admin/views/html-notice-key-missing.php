a262c351246f2d111ab46ec795a9759ff8e00f3a
Filipe Boldo <filipeboldo@MacBook-Pro-de-Filipe.local>
2019-01-22 11:46:45 +0000

    Better error reporting for Kamoney service

--- includes/admin/views/html-notice-key-missing.php
+++ includes/admin/views/html-notice-key-missing.php
@@ -10,6 +10,9 @@ if ( ! defined( 'ABSPATH' ) ) {
 ?>
 
 <div class="error inline">
-	<p><strong><?php _e( 'SmartCash Payments Disabled', 'wcscp' ); ?></strong>: <?php _e( 'You should inform a valid Kamoney Public and Secret Key.', 'wcscp' ); ?>
+	<p>
+		<strong><?php _e( 'SmartCash Payments Disabled', 'wcscp' ); ?></strong><br />
+		<?php _e( 'You should inform a valid Kamoney Public and Secret Key.', 'wcscp' ); ?><br />
+		- <?php echo $test["error"]; ?>
 	</p>
 </div>
